8daae20262e2f9c2ad8dfcacc3e9c7f80b985b36
Alex Dawn <al_4242@hotmail.co.uk>
2022-12-18 19:27:15 +0000

    Get fullscreen option working

--- docs/custom.js
+++ docs/custom.js
@@ -91,7 +91,13 @@ function pressReset() {
 }
 
 function pressResize() {
-
+    var canvas = document.getElementById("canvas");
+    if(canvas.requestFullScreen)
+        canvas.requestFullScreen();
+    else if(canvas.webkitRequestFullScreen)
+        canvas.webkitRequestFullScreen();
+    else if(canvas.mozRequestFullScreen)
+        canvas.mozRequestFullScreen();
 }
 
 function loadServerRom(filePath) {
